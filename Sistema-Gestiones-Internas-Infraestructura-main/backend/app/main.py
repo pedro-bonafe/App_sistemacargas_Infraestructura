caccc0ace319aff8f9827c03e6bc9104fab15364
labotech-analytics <labotech.analytics@gmail.com>
2026-01-16 22:42:48 -0300

    sumo dominio githubpages

--- Sistema-Gestiones-Internas-Infraestructura-main/backend/app/main.py
+++ Sistema-Gestiones-Internas-Infraestructura-main/backend/app/main.py
@@ -2,11 +2,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import me, gestiones, catalogos, usuarios
 
-app = FastAPI(title="Infra Gestión API")
+app = FastAPI(title="Infra Gestión API - BUILD 2026-01-17-001")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"^http://(localhost|127\.0\.0\.1)(:\d+)?$",
+    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8081", "http://127.0.0.1:8081", "https://pedro-bonafe.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
